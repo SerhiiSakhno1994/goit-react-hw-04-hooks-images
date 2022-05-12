207d63885c70625b7685a20fb294cb4f48269b88
Serhii <89796587+SerhiiSakhno1994@users.noreply.github.com>
2022-05-12 11:42:18 +0300

    done

--- src/components/ImageInfo/ImageInfo.jsx
+++ src/components/ImageInfo/ImageInfo.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Notiflix from 'notiflix';
 
 import FetchPixabay from 'components/services/pixabayApi';
@@ -10,32 +10,24 @@ import s from './ImageInfo.module.css';
 import Loader from 'components/Loader/Loader';
 import Modal from 'components/Modal/Modal';
 
-export default class ImageInfo extends Component {
-  state = {
-    hits: [],
-    picturesName: '',
-    page: 1,
-    total: 0,
-    largeImageURL: '',
-    isModal: false,
-    loader: false,
-    showModal: false,
-  };
+export default function ImageInfo() {
+  const [hits, setHits] = useState([]);
+  const [picturesName, setPicturesName] = useState('html');
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [loader, setIsLoader] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.picturesName !== this.state.picturesName ||
-      prevState.page !== this.state.page
-    ) {
-      this.fetchPicturesName();
-    }
-  }
+  useEffect(() => {
+    // if (picturesName === '') {
+    //   return;
+    // }
+    fetchPicturesName();
+  }, [page, picturesName]);
 
-  fetchPicturesName = () => {
-    const { picturesName, page } = this.state;
-    this.setState({
-      loader: true,
-    });
+  const fetchPicturesName = () => {
+    setIsLoader(true);
     FetchPixabay(picturesName, page).then(r => {
       if (r.totalHits === 0 && r.hits.length === 0) {
         Notiflix.Report.failure(
@@ -45,64 +37,146 @@ export default class ImageInfo extends Component {
         );
       }
 
-      this.setState(prevState => ({
-        hits: [...prevState.hits, ...r.hits],
-        total: r.total,
-        loader: false,
-      }));
+      setHits(state => [...state, ...r.hits]);
+      setTotal(r.total);
+      setIsLoader(false);
     });
   };
 
-  onSubmit = searchPicturesName => {
-    this.setState({
-      hits: [],
-      picturesName: searchPicturesName,
-      page: 1,
-    });
+  const onSubmit = searchPicturesName => {
+    setHits([]);
+    setPicturesName(searchPicturesName);
+    setPage(1);
   };
 
-  handleClick = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const handleClick = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  handleClickImg = largeImageURL => {
-    this.setState({
-      largeImageURL,
-      showModal: true,
-    });
+  const handleClickImg = largeImageURL => {
+    setLargeImageURL(largeImageURL);
+    setShowModal(true);
   };
 
-  toggleModal = () => {
-    this.setState({
-      showModal: false,
-    });
+  const toggleModal = () => {
+    setShowModal(false);
   };
 
-  render() {
-    const { hits, total, page, largeImageURL, loader, showModal } = this.state;
-    const loadMore =
-      total > page * 12 && loader ? (
-        <Loader color={'#3f51b5'} height={100} width={100} />
-      ) : (
-        total > page * 12 && (
-          <Button onClick={this.handleClick} text={'Load more'} />
-        )
-      );
-    return (
-      <div className={s.ImageInfo}>
-        <Searchbar onSubmit={this.onSubmit} />
-        {hits.length > 0 && (
-          <ImageGallery hits={hits} handleClickImg={this.handleClickImg} />
-        )}
-        {loadMore}
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <LargeImage largeImageURL={largeImageURL} />
-          </Modal>
-        )}
-      </div>
+  const loadMore =
+    total > page * 12 && loader ? (
+      <Loader color={'#3f51b5'} height={100} width={100} />
+    ) : (
+      total > page * 12 && <Button onClick={handleClick} text={'Load more'} />
     );
-  }
+  return (
+    <div className={s.ImageInfo}>
+      <Searchbar onSubmit={onSubmit} />
+      {hits.length > 0 && (
+        <ImageGallery hits={hits} handleClickImg={handleClickImg} />
+      )}
+      {loadMore}
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <LargeImage largeImageURL={largeImageURL} />
+        </Modal>
+      )}
+    </div>
+  );
 }
+
+// export default class ImageInfo extends Component {
+//   state = {
+//     hits: [],
+//     picturesName: '',
+//     page: 1,
+//     total: 0,
+//     largeImageURL: '',
+//     isModal: false,
+//     loader: false,
+//     showModal: false,
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (
+//       prevState.picturesName !== this.state.picturesName ||
+//       prevState.page !== this.state.page
+//     ) {
+//       this.fetchPicturesName();
+//     }
+//   }
+
+//   fetchPicturesName = () => {
+//     const { picturesName, page } = this.state;
+//     this.setState({
+//       loader: true,
+//     });
+//     FetchPixabay(picturesName, page).then(r => {
+//       if (r.totalHits === 0 && r.hits.length === 0) {
+//         Notiflix.Report.failure(
+//           'WARNING',
+//           `Sorry, there is no image named ${picturesName}`,
+//           'Close'
+//         );
+//       }
+
+//       this.setState(prevState => ({
+//         hits: [...prevState.hits, ...r.hits],
+//         total: r.total,
+//         loader: false,
+//       }));
+//     });
+//   };
+
+//   onSubmit = searchPicturesName => {
+//     this.setState({
+//       hits: [],
+//       picturesName: searchPicturesName,
+//       page: 1,
+//     });
+//   };
+
+//   handleClick = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//   };
+
+//   handleClickImg = largeImageURL => {
+//     this.setState({
+//       largeImageURL,
+//       showModal: true,
+//     });
+//   };
+
+//   toggleModal = () => {
+//     this.setState({
+//       showModal: false,
+//     });
+//   };
+
+//   render() {
+//     const { hits, total, page, largeImageURL, loader, showModal } = this.state;
+// const loadMore =
+//   total > page * 12 && loader ? (
+//     <Loader color={'#3f51b5'} height={100} width={100} />
+//   ) : (
+//     total > page * 12 && (
+//       <Button onClick={this.handleClick} text={'Load more'} />
+//     )
+//   );
+// return (
+//   <div className={s.ImageInfo}>
+//     <Searchbar onSubmit={this.onSubmit} />
+//     {hits.length > 0 && (
+//       <ImageGallery hits={hits} handleClickImg={this.handleClickImg} />
+//     )}
+//     {loadMore}
+//     {showModal && (
+//       <Modal onClose={this.toggleModal}>
+//         <LargeImage largeImageURL={largeImageURL} />
+//       </Modal>
+//     )}
+//   </div>
+// );
+//   }
+// }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,69 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
 import s from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handeleKeyDown);
-  }
+export default function Modal({ children, onClose }) {
+  useEffect(() => {
+    console.log('я визвався');
+    window.addEventListener('keydown', handeleKeyDown);
+    return () => {
+      console.log('Это функция очистки перед следующим вызовом useEffect');
+      window.removeEventListener('keydown', handeleKeyDown);
+    };
+  }, []);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handeleKeyDown);
-  }
-
-  handeleKeyDown = e => {
+  const handeleKeyDown = e => {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
-  handeleBackdropClick = e => {
+  const handeleBackdropClick = e => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
-
-  render() {
-    return createPortal(
-      <div className={s.Overlay} onClick={this.handeleBackdropClick}>
-        <div className={s.Modal}>{this.props.children}</div>
-      </div>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <div className={s.Overlay} onClick={handeleBackdropClick}>
+      <div className={s.Modal}>{children}</div>
+    </div>,
+    modalRoot
+  );
 }
-
 Modal.propTypes = {
   children: PropTypes.object.isRequired,
   onClose: PropTypes.func.isRequired,
 };
+
+// export default class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handeleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handeleKeyDown);
+//   }
+
+//   handeleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+//   handeleBackdropClick = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <div className={s.Overlay} onClick={this.handeleBackdropClick}>
+//         <div className={s.Modal}>{this.props.children}</div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
